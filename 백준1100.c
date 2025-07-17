5c0960dc5fd88629db6d0ab0e59a0fe11e8de1e4
junwoo <junjuns1106hyun@gmail.com>
2025-07-17 22:20:23 +0900

    Add files via upload

---
+++ 백준1100.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	char chessboard[9][9];
+	int cnt = 0;
+
+	for (int i = 0; i < 8; i++) {
+		scanf("%s", chessboard[i]);
+	}
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if ((i + j) % 2 == 0 && chessboard[i][j] == 'F')
+				cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
